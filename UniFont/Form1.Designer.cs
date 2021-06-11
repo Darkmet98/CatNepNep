ffaa55cfba3a4a75f843f51b7a482f9d93f242b6
Darkmet98 <darkmet98@gmail.com>
2021-06-11 10:11:13 +0200

    Implemented unifont

---
+++ UniFont/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace UniFont
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load Font";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::UniFont.Properties.Resources.font_u;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(118, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1148, 799);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 62);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Save Font";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1289, 837);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "UniFont";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ UniFont/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Yarhl.IO;
+
+namespace UniFont
+{
+    public partial class Form1 : Form
+    {
+        private int x = 128;
+        private int y = 72;
+        private int xSpacing = 72;
+        private int ySpacing = 81;
+        private int count = 16;
+        private int rows = 10;
+        private List<TextBox> textBoxes;
+        private DataStream stream;
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBoxes = new List<TextBox>();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //0x5
+            stream = DataStreamFactory.FromFile("WIDTH.bin", FileOpenMode.ReadWrite);
+            var reader = new DataReader(stream) {Stream = {Position = 0x5}};
+
+
+            
+            var yMod = y;
+
+            for (int i = 0; i < rows; i++)
+            {
+                var xMod = x;
+                for (int j = 0; j < count; j++)
+                {
+                    textBoxes.Add(new TextBox()
+                    {
+                        Location = new Point(xMod, yMod),
+                        Size = new Size(49, 10),
+                        Font = new Font(FontFamily.GenericSansSerif, 8),
+                        Text = reader.ReadByte().ToString(),
+                        TextAlign = HorizontalAlignment.Center
+
+                    });
+                    xMod += xSpacing;
+                }
+                yMod += ySpacing;
+            }
+
+            
+            Controls.AddRange(textBoxes.ToArray());
+            textBoxes.ForEach(x => x.BringToFront());
+            pictureBox1.SendToBack();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBoxes.Count == 0)
+            {
+                MessageBox.Show("You need first to load the font before saving.", "Font not loaded", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+            var writer = new DataWriter(stream) {Stream = {Position = 0x5}};
+            foreach (var box in textBoxes)
+            {
+                var val = Convert.ToInt32(box.Text);
+                if (val > 256)
+                    val = 255;
+                writer.Write(Convert.ToByte(val));
+            }
+            stream.WriteTo("WIDTH_NEW.bin");
+            MessageBox.Show("Font updated", "blablabla", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
