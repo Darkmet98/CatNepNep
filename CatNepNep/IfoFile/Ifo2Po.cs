1182861d68e3a63a85152c72ed74a0eb6ce54cc7
Darkmet98 <darkmet98@gmail.com>
2021-07-08 18:32:06 +0200

    Changed license GPL to MIT

--- CatNepNep/IfoFile/Ifo2Po.cs
+++ CatNepNep/IfoFile/Ifo2Po.cs
@@ -1,22 +1,4 @@
-﻿// Copyright (C) 2021 Pedro Garau Martínez
-//
-// This file is part of CatNepNep.
-//
-// CatNepNep is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// CatNepNep is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with CatNepNep. If not, see <http://www.gnu.org/licenses/>.
-//
-
-using Yarhl.FileFormat;
+﻿using Yarhl.FileFormat;
 using Yarhl.Media.Text;
 
 namespace CatNepNep.IfoFile
--- CatNepNep/Program.cs
+++ CatNepNep/Program.cs
@@ -1,22 +1,4 @@
-﻿// Copyright (C) 2019 Pedro Garau Martínez
-//
-// This file is part of CatNepNep.
-//
-// CatNepNep is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// CatNepNep is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with CatNepNep. If not, see <http://www.gnu.org/licenses/>.
-//
-
-using System;
+﻿using System;
 using System.IO;
 using System.Text;
 using CatNepNep.BinFile;
@@ -38,7 +20,7 @@ namespace CatNepNep
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(@"CatNepNep - A Neptunia spin offs toolkit for fan translations by Darkmet98. Version: 1.0");
+            Console.WriteLine(@"CatNepNep - A simple tool for translating HyperDimension Neptunia spin-off games and more by Darkmet98. Version: 1.0");
 
             // Make sure that the shift-jis encoding is initialized in
             // .NET Core
